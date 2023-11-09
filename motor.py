c8a7edd03f56dd6099938b4aabb642152a2aca98
MatsJKUL <mats.janssens@student.kuleuven.be>
2023-11-09 16:23:49 +0100

    servo working

--- motor.py
+++ motor.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)  # Set the GPIO mode to use the BCM numbering
-servo_pin = 17           # The GPIO pin connected to the servo
+servo_pin = 16           # The GPIO pin connected to the servo
 GPIO.setup(servo_pin, GPIO.OUT)
 
 # Create a PWM object with a frequency of 50 Hz
@@ -15,7 +15,7 @@ try:
     while True:
         # Move the servo to a specific angle by changing the duty cycle
         angle = float(input("Enter the angle (0 to 180 degrees): "))
-        duty_cycle = 2.5 + 10 * angle / 180  # Map the angle to the duty cycle
+        duty_cycle = 2.5 + 10 * angle / 270  # Map the angle to the duty cycle
         pwm.ChangeDutyCycle(duty_cycle)
         time.sleep(1)
 
