e7a6c45abd0e1505cb3823dcf84f737d2b2f66fe
NLCShifu <simon__adler@outlook.com>
2023-11-16 16:33:15 +0100

    last version

--- kansrekenen.py
+++ kansrekenen.py
@@ -1,23 +1,97 @@
-import matplotlib.pyplot as plt
+import random
 
-# Données à représenter dans le diagramme circulaire
-categories = ['Catégorie A', 'Catégorie B', 'Catégorie C', 'Catégorie D']
-valeurs = [30, 45, 15, 10]  # Les pourcentages ou les valeurs numériques
+class Players:
+    def __init__(self):
+        self.cards = []
+        self.points = 0
+        self.amount_of_aces = 0
+        self.add_points()
 
-# Couleurs pour chaque catégorie (optionnel)
-couleurs = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
+    def get_card(self, card):
+        self.cards.append(card)
+        self.add_points()
 
-# Explode (pour séparer les portions, optionnel)
-explode = (0.05, 0, 0, 0)
+    def add_points(self):
+        points = 0
+        amount_of_aces = 0
+        for card in self.cards:
+            if card[1] < 11 and 1 < card[1]:
+                points += card[1]
 
-# Crée le diagramme circulaire
-plt.figure(figsize=(8, 8))
-plt.pie(valeurs, explode=explode, labels=categories, colors=couleurs, autopct='%1.1f%%', shadow=False, startangle=140)
+            elif card[1] == 1:
+                amount_of_aces += 1
+                points += 11
+            else:
+                points += 10
+        self.points = points
+        self.amount_of_aces = amount_of_aces
+        self.total_points()
 
-# Titre du diagramme
-plt.title('Diagramme circulaire d\'exemple')
+    def total_points(self):
+        if self.points > 21:
+            if self.amount_of_aces > 0:
+                self.points -= 10
+                self.amount_of_aces -= 1
+                self.total_points()
 
-# Affiche le diagramme
-plt.axis('equal')  # Assure que le diagramme soit un cercle et non une ellipse
-plt.show()
+    def status(self):
+        return self.cards, self.points
+
+    colors = ["h", "d", "c", "s"]
+    Waarde = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+    cards = {(color, value) for color in colors for value in Waarde}
+    deleted_cards = []
+
+    def random_card_choice(cards, deleted_cards):
+        random_cards = random.choice(list(cards))
+        cards.remove(random_cards)
+        deleted_cards.append(random_cards)
+        return random_cards
+
+    def hit(player, cards, deleted_cards):
+        # in class when code is done
+        player.get_card(random_card_choice(cards, deleted_cards))
+
+    def double(player, cards, deleted_cards):
+        # in class when code is done
+        player.get_card(random_card_choice(cards, deleted_cards))
+
+def value_card(card):
+    if card[1] < 11 and 1 < card[1]:
+        return card[1]
+    elif card[1] == 1:
+        return 1
+    else:
+        return 10
+
+def kans_sucess_stand(player_cards,dealer_cards,player_point,dealer_point,cards):
+    card_dict = dict()
+    for card in list(cards):
+        if card[1] not in card_dict:
+            card_dict[card[1]] = 1
+        else:
+            card_dict[card[1]] = card_dict[card[1]] + 1
+    if player_point > 21:
+        return 0
+    elif player_point==21:
+        return 100
+    elif player_point < 17:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- test.py
+++ test.py
@@ -1,42 +1,60 @@
-class Players:
-    def __init__(self):
-        self.cards = []
-        self.points = 0
-        self.amount_of_aces = 0
-        self.add_points()
+def calculate_win_probability(player_cards, dealer_cards, player_point, dealer_point, remaining_deck):
+    # Create a dictionary to count the occurrences of each card rank in the remaining deck
+    card_dict = dict()
+    for card in remaining_deck:
+        rank = card[0]
+        if rank not in card_dict:
+            card_dict[rank] = 1
+        else:
+            card_dict[rank] += 1
 
-    def get_card(self, card):
-        self.cards.append(card)
-        self.add_points()
+    # Calculate the probability of the player going bust (going over 21)
+    bust_probability = 0
+    for card_rank in card_dict:
+        if player_point + card_values[card_rank] > 21:
+            bust_probability += card_dict[card_rank] / len(remaining_deck)
 
-    def add_points(self):
-        points = 0
-        amount_of_aces = 0
-        for card in self.cards:
-            if type(card[1]) is int:
-                points += card[1]
+    # Calculate the probability of the player getting a blackjack (21 points with 2 cards)
+    blackjack_probability = 0
+    if len(player_cards) == 2 and player_point == 21:
+        blackjack_probability = 1
 
-            elif card[1] == "ace":
-                amount_of_aces += 1
-                points += 11
-            else:
-                points += 10
-        self.points = points
-        self.amount_of_aces = amount_of_aces
-        self.total_points()
+    # Calculate the probability of the player winning by comparing to the dealer's final hand
+    win_probability = 0
+    if player_point <= 21:
+        while dealer_point < 17:
+            # Dealer hits until they have 17 or more points
+            for card_rank in card_dict:
+                if dealer_point + card_values[card_rank] <= 21:
+                    win_probability += (card_dict[card_rank] / len(remaining_deck)) * dealer_win_probability(dealer_point + card_values[card_rank])
 
-    def total_points(self):
-        if self.points > 21:
-            if self.amount_of_aces > 0:
-                self.points -= 10
-                self.amount_of_aces -= 1
-                self.total_points()
+    # Calculate the overall chance of winning
+    total_win_probability = (1 - bust_probability) * blackjack_probability + (1 - bust_probability) * win_probability
 
-    def status(self):
-        return self.cards, self.points
-player = Players()
-player.get_card([1,2])
-print(player.status())
-player = Players()
-player.get_card([3,4])
-print(player.status())
+    return total_win_probability
+
+# Example card values (you may define your own values)
+card_values = {
+    '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10,
+    'J': 10, 'Q': 10, 'K': 10, 'A': 11
+}
+
+def dealer_win_probability(dealer_point):
+    if dealer_point > 21:
+        return 0
+    elif dealer_point == 21:
+        return 100
+    elif dealer_point < 17:
+        return 0
+    else:
+        return 100
+
+# Example usage
+player_cards = [('A', 'Spades'), ('9', 'Hearts')]
+dealer_cards = [('K', 'Diamonds'), ('7', 'Clubs')]
+player_point = 20
+dealer_point = 17
+remaining_deck = [('2', 'Clubs'), ('3', 'Hearts'), ('4', 'Diamonds')]
+
+win_probability = calculate_win_probability(player_cards, dealer_cards, player_point, dealer_point, remaining_deck)
+print(f"Player's chance of winning: {win_probability:.2f}%")
