f6678e0c43e3ab30971001ef708d39c5b4badada
MatsJKUL <mats.janssens@student.kuleuven.be>
2023-11-30 15:41:45 +0100

    commited good

--- check_switch.py
+++ check_switch.py
@@ -2,11 +2,11 @@ import RPi.GPIO as gpio
 import time
 
 gpio.setmode(gpio.BCM)
-gpio.setup(21, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+gpio.setup(20, gpio.IN, pull_up_down=gpio.PUD_DOWN)
  
 while True:
-    print(gpio.input(21))
-    if gpio.input(21) == 1:     
+    print(gpio.input(20))
+    if gpio.input(20) == 1:     
         print("LOW")
         break
 
