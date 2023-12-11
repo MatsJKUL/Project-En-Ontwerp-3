6ec4765fbb51303e75774426917d482807fb3409
Mats-Janssens <mats.janssens@student.kuleuven.be>
2023-12-11 15:41:45 +0100

    modified bet renderer

--- collab_1.py
+++ collab_1.py
@@ -242,9 +242,7 @@ class Player:
         self.state = ''
 
     def get_bet(self):
-        print('get_bet')
         move = None
-        pygame.font.Font(None, 36).render(f"PLACE YOUR BET {self.name}", True, (50, 50, 50))
         while move != 'OK':
             if GPIO.input(21) == 1:
                 self.bet.append(5)
@@ -256,6 +254,36 @@ class Player:
                 move = recognise_hand()
         if len(self.bet) == 0:
             self.get_bet()
+
+    def game_ask_for_bet(self):
+        bet_render = pygame.font.Font(None, 36).render(f"PLACE YOUR BET {self.name}", True, (50, 50, 50))
+        bet_render_rect = bet_render.get_rect()
+
+        bet_render_rect.center = (
+            int(self.screen_width/2), int(self.screen_height/2))
+
+        pygame.draw.rect(
+            self.screen, self.background, bet_render_rect)
+        self.screen.blit(bet_render, bet_render_rect)
+
+        pygame.display.update()
+        self.clock.tick(30)
+
+
+    def game_unblit_ask_for_bet(self):
+        bet_render = pygame.font.Font(None, 36).render(f"PLACE YOUR BET {self.name}", True, (1, 150, 32))
+        bet_render_rect = bet_render.get_rect()
+
+        bet_render_rect.center = (
+            int(self.screen_width/2), int(self.screen_height/2))
+
+        pygame.draw.rect(
+            self.screen, self.background, bet_render_rect)
+        self.screen.blit(bet_render, bet_render_rect)
+
+        pygame.display.update()
+        self.clock.tick(30)
+
 
     def get_total_bet(self):
         return sum(self.bet)
